c8ebb9b7328ee4071750010b6b98d45275c49ab7
Jamie Prentice <jamie.c.prentice@gmail.com>
2024-12-07 17:01:15 +0000

    Initial commit

--- 2024/day_07.R
+++ 2024/day_07.R
@@ -8,7 +8,7 @@ input
 find_match <- function(goal, xs) {
     f <- function(rest, acc = 0) {
         if (length(rest) == 0) return(acc == goal)
-        
+
         f(rest[-1], acc + rest[[1]]) ||
             f(rest[-1], acc * rest[[1]])
     }
@@ -18,7 +18,7 @@ find_match <- function(goal, xs) {
 part1 <- map_dbl(input, \(x, i) {
     goal <- x |> str_split_i(": ", 1) |> as.numeric()
     xs <- x |> str_split_i(": ", 2) |> str_split_1(" ") |> as.numeric()
-    
+
     find_match(goal, xs) * goal
 }) |> sum()
 
@@ -31,7 +31,7 @@ ncat  <- function(x, y) x * 10^(floor(log10(y)) + 1) + y
 find_match2 <- function(goal, xs) {
     f <- function(rest, acc = 0) {
         if (length(rest) == 0) return(acc == goal)
-        
+
         f(rest[-1], acc + rest[[1]]) ||
             f(rest[-1], acc * rest[[1]]) ||
             f(rest[-1], ncat(acc, rest[[1]]))
@@ -42,7 +42,7 @@ find_match2 <- function(goal, xs) {
 part2 <- imap_dbl(input, \(x, i) {
     goal <- x |> str_split_i(": ", 1) |> as.numeric()
     xs <- x |> str_split_i(": ", 2) |> str_split_1(" ") |> as.numeric()
-    
+
     # message("Input ", i, ": goal = ", goal)
     find_match2(goal, xs) * goal
 }, .progress = TRUE) |> sum()
